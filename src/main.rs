ffbd0aaecaf7e981c60521e17b608c9284807076
Duncan Dean <duncangleeddean@gmail.com>
2022-07-31 22:34:06 +0200

    Add sig validation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::str::FromStr;
+
+use anyhow::{Context, Result};
 use clap::{CommandFactory, Parser, Subcommand};
 use rand::prelude::ThreadRng;
-use schnorr_fun::{KeyPair, fun::{nonce, Scalar}, Schnorr};
+use schnorr_fun::{
+    fun::{marker::Public, nonce, Scalar, XOnly},
+    KeyPair, Message, Schnorr, Signature,
+};
 use sha2::Sha256;
 
 #[derive(Parser)]
@@ -22,27 +28,54 @@ enum Commands {
     },
 }
 
-fn generate_keypair() -> KeyPair {
-    let nonce_gen = nonce::Synthetic::<Sha256, nonce::GlobalRng<ThreadRng>>::default();
-    let schnorr = Schnorr::<Sha256, _>::new(nonce_gen);
+fn generate_keypair(
+    schnorr: Schnorr<Sha256, nonce::Synthetic<Sha256, nonce::GlobalRng<ThreadRng>>>,
+) -> KeyPair {
     let secret_key = Scalar::random(&mut rand::thread_rng());
     schnorr.new_keypair(secret_key)
 }
 
-fn main() {
+fn new_schnorr() -> Schnorr<Sha256, nonce::Synthetic<Sha256, nonce::GlobalRng<ThreadRng>>> {
+    let nonce_gen = nonce::Synthetic::<Sha256, nonce::GlobalRng<ThreadRng>>::default();
+    Schnorr::<Sha256, _>::new(nonce_gen)
+}
+
+fn verify_signature(public_key: String, signature: String, message: String) -> Result<bool> {
+    let verification_key = XOnly::from_str(&public_key)
+        .context("public key invalid")?
+        .to_point();
+    let message_bytes = hex::decode(message)?;
+    let message = Message::<Public>::raw(&message_bytes);
+    let signature =
+        Signature::<Public>::from_str(&signature).context("signature could not be parsed")?;
+    let schnorr = new_schnorr();
+    Ok(schnorr.verify(&verification_key, message, &signature))
+}
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
     let mut cmd = Cli::command();
 
     match cli.command {
         Some(Commands::Generate {}) => {
-            let keypair = generate_keypair();
+            let keypair = generate_keypair(new_schnorr());
             println!("Public key: {}", keypair.public_key());
         }
-        Some(Commands::Verify { .. }) => {
-            // TODO: Verification
+        Some(Commands::Verify {
+            public_key,
+            signature,
+            message,
+        }) => {
+            if verify_signature(public_key, signature, message)? {
+                println!("Valid Schnorr signature!");
+            } else {
+                println!("Invalid Schnorr signature");
+            }
         }
         None => {
             cmd.print_help().expect("help should not fail");
         }
     }
+
+    Ok(())
 }
